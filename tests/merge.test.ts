4aa3b16e288c0a0c5765002e94ee786115d6951e
HE KYPTO <zoonyanapat@gmail.com>
2023-11-01 23:21:29 +0700

    init

---
+++ tests/merge.test.ts
@@ -0,0 +1,24 @@
+import merge from '../src/merge';
+
+describe('Merge Function', () => {
+    it('should merge two sorted arrays correctly', () => {
+        const collection1 = [1, 3, 5, 7];
+        const collection2 = [2, 4, 6, 8];
+        const merged = merge(collection1, collection2);
+        expect(merged).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+
+    it('should handle empty input arrays', () => {
+        const collection1: number[] = [];
+        const collection2: number[] = [];
+        const merged = merge(collection1, collection2);
+        expect(merged).toEqual([]);
+    });
+
+    it('should merge arrays with different lengths', () => {
+        const collection1 = [1, 3, 5];
+        const collection2 = [2, 4, 6, 8];
+        const merged = merge(collection1, collection2);
+        expect(merged).toEqual([1, 2, 3, 4, 5, 6, 8]);
+    });
+});
